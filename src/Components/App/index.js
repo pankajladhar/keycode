5391a5305b4e5cc690a3d66d3bc9ddb54d0109ca
pankajladhar <ladharpankaj@gmail.com>
2018-08-19 17:22:07 +0530

    Added logo svg

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      keyCode: "Press key"
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            keyCode: "Press key"
+        };
+    }
 
-  componentDidMount() {
-    document.addEventListener('keydown', (e) => {
-      this.setState({
-        keyCode: e.keyCode,
-        key: e.key,
-        which:e.which,
-        code: e.code
-      })
-    })
-  }
+    componentDidMount() {
+        document.addEventListener('keydown', (e) => {
+            this.setState({
+                keyCode: e.keyCode,
+                key: e.key,
+                which: e.which,
+                code: e.code
+            })
+        })
+    }
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown')
-  }
+    componentWillUnmount() {
+        document.removeEventListener('keydown')
+    }
 
-  render() {
-    const { keyCode, key,which, code} = this.state
-    return (
-      <div className="App">
-        <span className="App__KeyCode">{keyCode}</span>
-        {keyCode !== "Press key" && <div className="Box__Container">
-          <div className="Box">
-            <h2>e.key</h2>
-            <span>{key}</span>
-          </div>
-          <div className="Box">
-            <h2>e.which</h2>
-            <span>{which}</span>
-          </div>
-          <div className="Box">
-            <h2>e.code</h2>
-            <span>{code}</span>
-          </div>
-        </div>}
-      </div>
-    );
-  }
+    render() {
+        const { keyCode, key, which, code } = this.state
+        return (
+            <div className="App">
+                <span className="App__KeyCode">{keyCode}</span>
+                {keyCode !== "Press key" && <div className="Box__Container">
+                    <div className="Box">
+                        <h2>e.key</h2>
+                        <span>{key}</span>
+                    </div>
+                    <div className="Box">
+                        <h2>e.which</h2>
+                        <span>{which}</span>
+                    </div>
+                    <div className="Box">
+                        <h2>e.code</h2>
+                        <span>{code}</span>
+                    </div>
+                </div>}
+            </div>
+        );
+    }
 }
 
 export default App
